f4c8b1a12630dc81ba7256f372b08495099a3707
Valentina Lazaro <104407130+ValexLaz@users.noreply.github.com>
2023-07-15 16:41:03 +0000

    Mis cambios random en el portafolio

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,13 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Valentina Lazaro",
+  title: "Beta Microsoft Learn Student Ambassador",
+  email: "valentina.lazaro@studentambassadors.com",
+  gitHub: "ValexLaz",
+  instagram: "valee.lazaro",
+  linkedIn: "valentina_lazaro",
+  twitter: "valentina_lazaro",
 };
 
 const primaryColor = "#4E567E";
